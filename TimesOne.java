3380f42ee02351dbb965af196d335f2af56be81b
SonarBoy <Joshua.L99.C@gmail.com>
2013-10-20 15:12:25 -0400

    First Commit

---
+++ TimesOne.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TimesOne{
+	public static void main(String [] args){
+
+		Scanner read = new Scanner(System.in);
+
+
+		System.out.print("Enter a Number: ");
+		int number = read.nextInt();
+
+		for(int i = 0; i < 10; i++){
+			System.out.println(number);
+
+		}
+
+
+
+
+	}
+}
