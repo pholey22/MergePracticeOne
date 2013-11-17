76415e936b0098dde10d50dd632b3c801768e155
Pholey22 <user@ubuntu.ubuntu-domain>
2013-11-16 20:32:50 -0500

    update

--- TimesOne.java
+++ TimesOne.java
@@ -8,9 +8,11 @@ public class TimesOne{
 
 		System.out.print("Enter a Number: ");
 		int number = read.nextInt();
+		System.out.print("Now, enter a word: ");
+		String word = read.next();
 
 		for(int i = 0; i < 10; i++){
-			System.out.println(number);
+			System.out.println(number+", "+word);
 
 		}
 
